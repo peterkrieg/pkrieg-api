687c9e650caafef2220eba4e72b8efc7adb5e209
Peter Krieg <peter.krieg@logikcull.com>
2019-11-08 10:04:22 -0500

    generated more fake data, and implemented offset / search term with api

--- lambda/index.js
+++ lambda/index.js
@@ -1,24 +1,45 @@
+/* eslint-disable indent */
 const corsHeaders = {
 	'Access-Control-Allow-Origin': '*', // Required for CORS support to work
 	'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
 };
 
-const projects = require('./projects.json');
+const allProjects = require('./projects.json');
 
 exports.handler = async (event, context) => {
-	console.log(JSON.stringify(event));
-	console.log(event);
+	// num to return at a time
+	const numProjectsToReturn = 20;
 
-	const { queryStringParameters } = event;
+	const queryStringParameters = event.queryStringParameters || {};
 
-	// searchTerm passed in as url param
-	const searchTerm = queryStringParameters && queryStringParameters.searchTerm;
+	// searchTerm, offset passed in as url parameters
+	const offset = +queryStringParameters.offset || 0;
+	const { searchTerm = '' } = queryStringParameters;
+
+	const filteredProjects = allProjects.filter(doesProjectMatchSearchTerm);
+	const projectsToReturn = filteredProjects.slice(offset, offset + numProjectsToReturn);
+
+
+	function doesProjectMatchSearchTerm (project) {
+		// this is terribly unoptimized, if no search term it stil loops through everything
+		if (!searchTerm) return true;
+
+		return project.name.toLowerCase().includes(searchTerm.toLowerCase());
+	}
+
+	const responseBody = {
+		searchTerm,
+		offset,
+		projects: projectsToReturn,
+		numProjects: filteredProjects.length,
+	};
+
 
 	const response = {
 		isBase64Encoded: false,
 		statusCode: 200,
 		headers: corsHeaders,
-		body: JSON.stringify({ searchTerm, ...projects }),
+		body: JSON.stringify(responseBody)
 	};
 
 
